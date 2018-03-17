ffdacf0aa6ef0654c797508c3ea11f287a3318ad
PINGUET Julien <pinguet62@gmail.com>
2018-03-17 16:54:18 +0100

    Polish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>App</div>
+  `
 })
 export class AppComponent {
-  title = 'app';
 }
--- src/app/router.ts
+++ src/app/router.ts
@@ -1,10 +1,10 @@
 import {Routes} from '@angular/router';
 
-import {LoginComponent} from './login/login.component';
 import {ArticleEditComponent} from './article/article-edit.component';
 import {ArticleListComponent} from './article/article-list.component';
 import {EmptyComponent} from './layout/empty.component';
 import {RouterOutletComponent} from './layout/router-outlet.component';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
   {path: '', component: EmptyComponent},
