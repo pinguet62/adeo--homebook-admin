99521f47e57340506487b4317a4a6165ecd64edb
PINGUET Julien <pinguet62@gmail.com>
2018-03-28 13:23:00 +0200

    Index: format

--- src/app/index.component.ts
+++ src/app/index.component.ts
@@ -3,11 +3,19 @@ import {Title} from '@angular/platform-browser';
 
 @Component({
   template: `
-    <div>
-      <button routerLink="/article" mat-raised-button>Articles</button>
-      <button routerLink="/user" mat-raised-button>Users</button>
+    <div class="menus">
+      <div class="box" routerLink="/article">
+        <img src="http://www.nerdglaze.com/wp-content/uploads/2013/12/Shelf-of-Books-1024x637.jpg">
+        <h2>Articles</h2>
+      </div>
+
+      <div class="box" routerLink="/user">
+        <img src="http://www.cerc.co.uk/software-support/assets/img/CERC_UGM_2010_097.jpg">
+        <h2>User</h2>
+      </div>
     </div>
-  `
+  `,
+  styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
 
