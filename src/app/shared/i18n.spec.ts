dd054c883cdf4f720e443ed5e541558714e8e656
PINGUET Julien <pinguet62@gmail.com>
2018-05-09 23:45:51 +0200

    i18n: use current locale + fix

---
+++ src/app/shared/i18n.spec.ts
@@ -0,0 +1,51 @@
+import {HttpErrorResponse} from '@angular/common/http';
+import {TranslateService} from '@ngx-translate/core';
+import {of, throwError} from 'rxjs';
+
+import {i18nInitializer} from './i18n';
+
+describe('shared/i18n', () => {
+  describe(i18nInitializer.name, () => {
+    const CULTURE_LANG = 'fr-FR';
+    const LANG = 'fr';
+
+    let translateService: jasmine.SpyObj<TranslateService>;
+    beforeEach(() => {
+      translateService = jasmine.createSpyObj(TranslateService.name, Object.keys(TranslateService.prototype));
+      translateService.getBrowserCultureLang.and.returnValue(CULTURE_LANG);
+      translateService.getBrowserLang.and.returnValue(LANG);
+    });
+
+    it('First, if browser "language + country" available, should use this lang', async () => {
+      const dataCultureLang = {foo: 'bar'};
+
+      translateService.use.and.callFake((lang) => {
+        if (lang === CULTURE_LANG) {
+          return of(dataCultureLang);
+        } else {
+          throw new Error('Should not occur');
+        }
+      });
+
+      const result = await i18nInitializer(translateService)();
+
+      expect(result).toEqual(dataCultureLang);
+    });
+
+    it('Second, should use browser "language"', async () => {
+      const dataLang = {foo: 'bar'};
+
+      translateService.use.and.callFake((lang) => {
+        if (lang === CULTURE_LANG) {
+          return throwError(new HttpErrorResponse({}));
+        } else if (lang === LANG) {
+          return of(dataLang);
+        }
+      });
+
+      const result = await i18nInitializer(translateService)();
+
+      expect(result).toEqual(dataLang);
+    });
+  });
+});
--- src/app/shared/i18n.ts
+++ src/app/shared/i18n.ts
@@ -1,10 +1,18 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {EMPTY, of} from 'rxjs';
+import {catchError, first, mergeMap} from 'rxjs/operators';
 
 export function i18nInitializer(translateService: TranslateService): () => Promise<any> {
   return () => {
     translateService.setDefaultLang('en');
-    return Promise.resolve();
+
+    // first lang who doesn't fail
+    return of(translateService.getBrowserCultureLang(), translateService.getBrowserLang()).pipe(
+      mergeMap(lang => translateService.use(lang).pipe(
+        catchError(() => EMPTY))),
+      first()
+    ).toPromise();
   };
 }
 
