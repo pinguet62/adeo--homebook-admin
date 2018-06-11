db8a3091ade3031189232e167319ba777a6f81f0
PINGUET Julien <pinguet62@gmail.com>
2018-06-11 23:51:27 +0200

    Prod: fix app URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.homebookplatform.com',
-  appUrl: 'https://homebookplatform.com'
+  appUrl: 'https://app.homebookplatform.com'
 };
